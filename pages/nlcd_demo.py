6a52af20dba69b9b7a61aff44346f723db2fa9aa
Carissa DeRanek <caderanek@g.ucla.edu>
2024-07-08 13:17:29 -0700

    Update nlcd_demo.py

--- pages/nlcd_demo.py
+++ pages/nlcd_demo.py
@@ -4,8 +4,10 @@ import streamlit as st
 import geemap.foliumap as geemap
 from google.oauth2 import service_account
 from ee import oauth
+from ipyleaflet import *
+from bqplot import pyplot as plt
 
-# Inistialize EE
+# Initialize EE helper fn
 def ee_initialize(force_use_service_account=False):
     if force_use_service_account or "json_data" in st.secrets:
         json_credentials = st.secrets["json_data"]
@@ -22,47 +24,79 @@ def ee_initialize(force_use_service_account=False):
 # Initialize GEE
 ee_initialize(force_use_service_account=True)
 
-# Get an NLCD image by year.
-def getNLCD(year):
-    # Import the NLCD collection.
-    dataset = ee.ImageCollection("USGS/NLCD_RELEASES/2019_REL/NLCD")
+# Get AOP data
+NEON_SITE = 'SOAP'
+# Import the AOP surface directional reflectance (SDR)
+aopSDR = ee.ImageCollection('projects/neon-prod-earthengine/assets/DP3-30006-001')
+site_2021_sdr = aopSDR \
+  .filterDate('2021-01-01', '2021-12-31') \
+  .filterMetadata('NEON_SITE', 'equals', NEON_SITE) \
+  .first()
 
-    # Filter the collection by year.
-    nlcd = dataset.filter(ee.Filter.eq("system:index", year)).first()
+# Select the WL_FWHM_B*** band properties (using regex)
+properties = site_2021_sdr.toDictionary()
+wl_fwhm_dict = properties.select(['WL_FWHM_B+\d{3}'])
 
-    # Select the land cover band.
-    landcover = nlcd.select("landcover")
-    return landcover
+# Pull out the wavelength, fwhm values to a list
+wl_fwhm_list = wl_fwhm_dict.values()
 
+# Function to pull out the wavelength values only and convert the string to float
+def get_wavelengths(x):
+  str_split = ee.String(x).split(',')
+  first_elem = ee.Number.parse((str_split.get(0)))
+  return first_elem
 
-st.header("National Land Cover Database (NLCD)")
+# apply the function to the wavelength full-width-half-max list
+wavelengths = wl_fwhm_list.map(get_wavelengths)
+
+# Create page layout
+st.header("National Ecological Observatory Network (NEON) Airborne Observation Platform (AOP) reflectance data")
 
 # Create a layout containing two columns, one for the map and one for the layer dropdown list.
 row1_col1, row1_col2 = st.columns([3, 1])
 
 # Create an interactive map
-Map = geemap.Map(center=[40, -100], zoom=4)
+m = geemap.Map()
+m.default_style = {"cursor": "crosshair"}
 
-# Select the seven NLCD epochs after 2000.
-years = ["2001", "2004", "2006", "2008", "2011", "2013", "2016", "2019"]
 
-# Add a dropdown list and checkbox to the second column.
-with row1_col2:
-    selected_year = st.multiselect("Select a year", years)
-    add_legend = st.checkbox("Show legend")
+# Update map
+with row1_col1:
+    m.centerObject(site_2021_sdr)
+    
+    figure = plt.figure(
+        1,
+        title="Reflectance",
+        layout={"height": "200px", "width": "600px"},
+    )
+    
+    
+    # Set up variables for plotting markers, reflectance
+    x = wavelengths.getInfo()
+    coordinates = []
+    markers = []
+    marker_cluster = MarkerCluster(name="Marker Cluster")
+    m.add_layer(marker_cluster)
+    
+    # Helper function called each time we click a new point
+    def handle_interaction(**kwargs):
+        latlon = kwargs.get("coordinates")
+        if kwargs.get("type") == "click":
+          try:
+            coordinates.append(latlon)
+            markers.append(Marker(location=latlon))
+            marker_cluster.markers = markers
+            xy = ee.Geometry.Point(latlon[::-1])
+            y = site_2021_sdr.select('B.*').sample(xy, 1).first().toDictionary().values().getInfo()
+            plt.clear()
+            plt.plot(x, y)
+          except:
+            print('Please click a point inside the TEAK box.')
+    
+    
+    m.on_interaction(handle_interaction)
+    
+    fig_control = WidgetControl(widget=figure, position="bottomright")
+    m.add_control(fig_control)
 
-# Add selected NLCD image to the map based on the selected year.
-if selected_year:
-    for year in selected_year:
-        Map.addLayer(getNLCD(year), {}, "NLCD " + year)
-
-    if add_legend:
-        Map.add_legend(
-            title="NLCD Land Cover Classification", builtin_legend="NLCD"
-        )
-    with row1_col1:
-        Map.to_streamlit(height=600)
-
-else:
-    with row1_col1:
-        Map.to_streamlit(height=600)
+    m.to_streamlit(height=600)
