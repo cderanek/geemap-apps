fa7bada7036c17af275ec1acbd7c9365cfbdace0
Carissa DeRanek <caderanek@g.ucla.edu>
2024-07-08 14:32:04 -0700

    Update nlcd_demo.py

--- pages/nlcd_demo.py
+++ pages/nlcd_demo.py
@@ -5,8 +5,8 @@ import geemap
 import geemap.colormaps as cm
 from google.oauth2 import service_account
 from ee import oauth
-from ipyleaflet import *
-from bqplot import pyplot as plt
+import folium
+import matplotlib.pyplot as plt
 
 # Initialize EE helper fn
 def ee_initialize(force_use_service_account=False):
@@ -22,80 +22,74 @@ def ee_initialize(force_use_service_account=False):
     else:
         ee.Initialize()
 
-# Helper function called each time we click a new point
-def handle_interaction(**kwargs):
-    latlon = kwargs.get("coordinates")
-    if kwargs.get("type") == "click":
-      try:
-        coordinates.append(latlon)
-        markers.append(Marker(location=latlon))
-        marker_cluster.markers = markers
-        xy = ee.Geometry.Point(latlon[::-1])
-        y = site_2021_sdr.select('B.*').sample(xy, 1).first().toDictionary().values().getInfo()
-        plt.clear()
-        plt.plot(x, y)
-      except:
-        print('Please click a point inside the TEAK box.')
-        
-# Initialize GEE
-ee_initialize(force_use_service_account=True)
-
 # Get AOP data
-NEON_SITE = 'SOAP'
-# Import the AOP surface directional reflectance (SDR)
-aopSDR = ee.ImageCollection('projects/neon-prod-earthengine/assets/DP3-30006-001')
-site_2021_sdr = aopSDR \
-  .filterDate('2021-01-01', '2021-12-31') \
-  .filterMetadata('NEON_SITE', 'equals', NEON_SITE) \
-  .first()
+def get_AOP_data():
+    NEON_SITE = 'SOAP'
+    # Import the AOP surface directional reflectance (SDR)
+    aopSDR = ee.ImageCollection('projects/neon-prod-earthengine/assets/DP3-30006-001')
+    site_2021_sdr = aopSDR \
+      .filterDate('2021-01-01', '2021-12-31') \
+      .filterMetadata('NEON_SITE', 'equals', NEON_SITE) \
+      .first()
+    return site_2021_sdr
 
-# Select the WL_FWHM_B*** band properties (using regex)
-properties = site_2021_sdr.toDictionary()
-wl_fwhm_dict = properties.select(['WL_FWHM_B+\d{3}'])
-
-# Pull out the wavelength, fwhm values to a list
-wl_fwhm_list = wl_fwhm_dict.values()
-
-# Function to pull out the wavelength values only and convert the string to float
-def get_wavelengths(x):
-  str_split = ee.String(x).split(',')
-  first_elem = ee.Number.parse((str_split.get(0)))
-  return first_elem
-
-# apply the function to the wavelength full-width-half-max list
-wavelengths = wl_fwhm_list.map(get_wavelengths)
-
-# Create page layout
-st.header("National Ecological Observatory Network (NEON) Airborne Observation Platform (AOP) reflectance data")
-
-# Create a layout containing two columns, one for the map and one for the layer dropdown list.
-row1_col1, row1_col2 = st.columns([3, 1])
-
-# Create an interactive map
-m = geemap.Map()
-
-# Update map
-# with row1_col1:
-m.default_style = {"cursor": "crosshair"}
-m.centerObject(site_2021_sdr)
+def get_wavelengths(site_2021_sdr):
+    # Select the WL_FWHM_B*** band properties (using regex)
+    properties = site_2021_sdr.toDictionary()
+    wl_fwhm_dict = properties.select(['WL_FWHM_B+\d{3}'])
     
-figure = plt.figure(
-    1,
-    title="Reflectance",
-    layout={"height": "200px", "width": "600px"},
-)
+    # Pull out the wavelength, fwhm values to a list
+    wl_fwhm_list = wl_fwhm_dict.values()
     
+    # Function to pull out the wavelength values only and convert the string to float
+    def get_wavelengths(x):
+      str_split = ee.String(x).split(',')
+      first_elem = ee.Number.parse((str_split.get(0)))
+      return first_elem
     
-# Set up variables for plotting markers, reflectance
-x = wavelengths.getInfo()
-coordinates = []
-markers = []
-marker_cluster = MarkerCluster(name="Marker Cluster")
-m.add_layer(marker_cluster)
-    
-m.on_interaction(handle_interaction)
-    
-fig_control = WidgetControl(widget=figure, position="bottomright")
-m.add_control(fig_control)
+    # apply the function to the wavelength full-width-half-max list
+    wavelengths = wl_fwhm_list.map(get_wavelengths)
 
-m.to_streamlit(height=600)
+    return wavelengths
+
+# Function to create Folium map
+def create_folium_map():
+    m = folium.Map(location=[0, 0], zoom_start=2)
+    return m
+
+# Function to handle map click events
+def handle_click(latlon):
+    try:
+        xy = ee.Geometry.Point(latlon[::-1])
+        x = get_wavelengths(site_2021_sdr)
+        y = site_2021_sdr.select('B.*').sample(xy, 1).first().toDictionary().values().getInfo()
+        plt.figure(figsize=(8, 4))
+        plt.plot(x, y)
+        plt.xlabel('Wavelength')
+        plt.ylabel('Reflectance')
+        plt.title('Reflectance Spectrum')
+        st.pyplot(plt)
+    except Exception as e:
+        st.error(f"Error: {e}")
+
+def main():
+    # Initialize GEE
+    ee_initialize(force_use_service_account=True)
+
+    # Import data
+    site_2021_sdr = get_AOP_data()
+    
+    # Create page layout
+    st.header("National Ecological Observatory Network (NEON) Airborne Observation Platform (AOP) reflectance data")
+
+    # Create a layout containing two columns, one for the map and one for the layer dropdown list.
+    row1_col1, row1_col2 = st.columns([3, 1])
+
+    # Create a Folium map
+    folium_map = create_folium_map()
+    folium_static(folium_map)
+
+    # Click event handler for Folium map
+    if folium_map.click_lat_lng:
+        handle_click(folium_map.click_lat_lng)
+
