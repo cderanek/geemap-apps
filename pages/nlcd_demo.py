9f09ad7ec4de9a8f290bff9e6fdacdf0e175ee85
Carissa DeRanek <caderanek@g.ucla.edu>
2024-07-08 13:38:54 -0700

    Update nlcd_demo.py

--- pages/nlcd_demo.py
+++ pages/nlcd_demo.py
@@ -21,6 +21,21 @@ def ee_initialize(force_use_service_account=False):
         ee.Initialize(credentials)
     else:
         ee.Initialize()
+
+# Helper function called each time we click a new point
+def handle_interaction(**kwargs):
+    latlon = kwargs.get("coordinates")
+    if kwargs.get("type") == "click":
+      try:
+        coordinates.append(latlon)
+        markers.append(Marker(location=latlon))
+        marker_cluster.markers = markers
+        xy = ee.Geometry.Point(latlon[::-1])
+        y = site_2021_sdr.select('B.*').sample(xy, 1).first().toDictionary().values().getInfo()
+        plt.clear()
+        plt.plot(x, y)
+      except:
+        print('Please click a point inside the TEAK box.')
         
 # Initialize GEE
 ee_initialize(force_use_service_account=True)
@@ -56,13 +71,11 @@ st.header("National Ecological Observatory Network (NEON) Airborne Observation P
 # Create a layout containing two columns, one for the map and one for the layer dropdown list.
 row1_col1, row1_col2 = st.columns([3, 1])
 
-# Create an interactive map
-m = geemap.Map()
-m.default_style = {"cursor": "crosshair"}
-
-
 # Update map
 with row1_col1:
+    # Create an interactive map
+    m = geemap.Map()
+    m.default_style = {"cursor": "crosshair"}
     m.centerObject(site_2021_sdr)
     
     figure = plt.figure(
@@ -77,23 +90,7 @@ with row1_col1:
     coordinates = []
     markers = []
     marker_cluster = MarkerCluster(name="Marker Cluster")
-    #m.add_layer(marker_cluster)
-    
-    # Helper function called each time we click a new point
-    def handle_interaction(**kwargs):
-        latlon = kwargs.get("coordinates")
-        if kwargs.get("type") == "click":
-          try:
-            coordinates.append(latlon)
-            markers.append(Marker(location=latlon))
-            marker_cluster.markers = markers
-            xy = ee.Geometry.Point(latlon[::-1])
-            y = site_2021_sdr.select('B.*').sample(xy, 1).first().toDictionary().values().getInfo()
-            plt.clear()
-            plt.plot(x, y)
-          except:
-            print('Please click a point inside the TEAK box.')
-    
+    m.add_layer(marker_cluster)
     
     m.on_interaction(handle_interaction)
     
